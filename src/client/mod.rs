af224b4ad42a508e0a77bad82d47511a5934bf81
J/A <archer884@gmail.com>
2017-07-13 12:59:33 -0500

    Add some comments.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -9,10 +9,18 @@ pub use client::youtube::YoutubeClient;
 
 pub trait Client {
     type Video: Video;
+
+    /// Queries the service for a video matching the provided identifier.
     fn query(&self, identifier: &str) -> Result<Self::Video>;
 }
 
 pub trait ClientConnector {
     type Err: error::Error + 'static;
+
+    /// Downloads the provided uri as a string.
+    ///
+    /// This is provided to allow the client to download video information, but bitreel does not 
+    /// provide an implementation so that consumers can use whatever network implementation they 
+    /// prefer.
     fn download_string(&self, uri: &str) -> result::Result<String, Self::Err>;
 }
